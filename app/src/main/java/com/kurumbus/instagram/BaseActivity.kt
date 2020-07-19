cac6a8cae2585ca5105cdc8b639fdd3c89c2fdea
kurumbus <grigory.shein@gmail.com>
2020-07-19 11:41:21 +0700

    transition between activities

---
+++ app/src/main/java/com/kurumbus/instagram/BaseActivity.kt
@@ -0,0 +1,47 @@
+package com.kurumbus.instagram
+
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.LabelVisibilityMode
+import kotlinx.android.synthetic.main.activity_home.*
+
+abstract class BaseActivity (val navNumber: Int) : AppCompatActivity(){
+
+    private val TAG = "BaseActivity"
+
+    fun setupBottomNavigation() {
+        bottom_navigation_view.setItemHorizontalTranslationEnabled(false) // for enableItemShiftingMode(false)
+        bottom_navigation_view.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED)  // for enableShiftingMode(false) // for enableItemShiftingMode(false)
+        bottom_navigation_view.setIconSize(29f, 29f)  // for enableShiftingMode(false) // for enableItemShiftingMode(false)
+        bottom_navigation_view.enableAnimation(false)
+        for (i in 0  until bottom_navigation_view.menu.size()) {
+            bottom_navigation_view.setIconTintList(i,null)
+        }
+
+        bottom_navigation_view.setOnNavigationItemSelectedListener {
+            val nextActivity = when (it.itemId) {
+                R.id.nav_item_home -> HomeActivity::class.java
+                R.id.nav_item_search -> SearchActivity::class.java
+                R.id.nav_item_share -> ShareActivity::class.java
+                R.id.nav_item_likes -> LikesActivity::class.java
+                R.id.nav_item_profile -> ProfileActivity::class.java
+                else -> {
+                    Log.e(TAG, "Unknown item clicked $it")
+                    null
+                }
+            }
+            if (nextActivity != null) {
+                val intent = Intent (this, nextActivity)
+                intent.flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
+                startActivity(intent)
+                overridePendingTransition(0,0)
+                true
+            } else {
+                false
+            }
+        }
+        bottom_navigation_view.menu.getItem(navNumber).isChecked = true
+
+    }
+ }
--- app/src/main/java/com/kurumbus/instagram/LikesActivity.kt
+++ app/src/main/java/com/kurumbus/instagram/LikesActivity.kt
@@ -3,18 +3,17 @@ package com.kurumbus.instagram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.content.Intent
 import com.google.android.material.bottomnavigation.LabelVisibilityMode
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : BaseActivity(0) {
-    private val TAG = "HomeActivity"
+class LikesActivity : BaseActivity(3) {
+    private val TAG = "LikesActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
         setupBottomNavigation()
         Log.d(TAG, "onCreate")
-
     }
 }
