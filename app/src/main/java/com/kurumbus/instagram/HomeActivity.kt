cabde40e39d0f385260224f0a41f15130b73ef00
kurumbus <grigory.shein@gmail.com>
2020-07-30 16:34:28 +0700

    #9 login screen

--- app/src/main/java/com/kurumbus/instagram/HomeActivity.kt
+++ app/src/main/java/com/kurumbus/instagram/HomeActivity.kt
@@ -1,26 +1,36 @@
 package com.kurumbus.instagram
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setupBottomNavigation()
         Log.d(TAG, "onCreate")
-
-        val auth = FirebaseAuth.getInstance()
-        auth.signInWithEmailAndPassword("grigory.shein@gmail.com", "orion123")
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signOut()
+        /*auth.signInWithEmailAndPassword("grigory.shein@gmail.com", "orion123")
             .addOnCompleteListener{
                 if (it.isSuccessful) {
                     Log.d(TAG, "Sign in successful")
                 } else {
                     Log.d(TAG, "Sign in failure", it.exception)
                 }
-            }
+            }*/
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mAuth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/kurumbus/instagram/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.kurumbus.instagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class LoginActivity : AppCompatActivity() {
+
+    private val TAG = "LoginActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        Log.d(TAG, "onCreate")
+    }
+}
