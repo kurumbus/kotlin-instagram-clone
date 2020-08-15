4494f98c039f4979788962af63ba6deb1f4575a4
kurumbus <grigory.shein@gmail.com>
2020-08-15 09:40:04 +0700

    #10 login screen improvements

--- app/src/main/java/com/kurumbus/instagram/LoginActivity.kt
+++ app/src/main/java/com/kurumbus/instagram/LoginActivity.kt
@@ -2,9 +2,16 @@ package com.kurumbus.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_login.*
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, TextWatcher {
 
     private val TAG = "LoginActivity"
 
@@ -12,5 +19,33 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         Log.d(TAG, "onCreate")
+        KeyboardVisibilityEvent.setEventListener(this, this)
+        login_button.isEnabled = false
+
+        email_input.addTextChangedListener(this)
+        password_input.addTextChangedListener(this)
     }
+
+    override fun onVisibilityChanged(isKeyboardOpen: Boolean) {
+        if (isKeyboardOpen) {
+            scroll_view.scrollTo(0, scroll_view.bottom)
+            create_account_text.visibility = View.GONE
+        } else {
+            scroll_view.scrollTo(0, scroll_view.top)
+            create_account_text.visibility = View.VISIBLE
+        }
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        login_button.isEnabled =
+                    email_input.text.toString().isNotEmpty() &&
+                    password_input.text.toString().isNotEmpty()
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    }
+
 }
